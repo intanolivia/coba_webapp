2f2db5ae4db251557145012fc9673a5e72636091
chocolivia <128565993+intanolivia@users.noreply.github.com>
2023-12-03 09:57:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,16 +38,9 @@ if page_cinema == "Edit Cinema Schedule":
 
     # Display existing cinema schedule data with options to edit or delete
     data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
-    for _, result in data.iterrows():        
-        id = result['id']
-        movie_title_lama = result["movie_title"]
-        genre_lama = result["genre"]
-        director_name_lama = result["director"]
-        release_date_lama = result["release_date"]
-        start_time_lama = result["start_time"]
-        end_time_lama = result["end_time"]
-        theater_number_lama = result["theater_number"]
-        ticket_price_lama = result["ticket_price"]
+        result_cinema = session_cinema.execute('SELECT * FROM movie_schedule ORDER BY id;')
+        for row in result_cinema:
+            id, movie_title_lama, genre_lama, director_lama, release_date_lama, start_time_lama, end_time_lama, theater_number_lama, ticket_price_lama = row
             
         with st.expander(f'{movie_title_lama}'):
                 with st.form(f'movie-data-{id}'):
