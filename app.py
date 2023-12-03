7c7830240185182e1b6032006d44aaa0c87b7c13
chocolivia <128565993+intanolivia@users.noreply.github.com>
2023-12-03 09:42:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ if page_cinema == "Edit Cinema Schedule":
             session_cinema.commit()
 
     # Display existing cinema schedule data with options to edit or delete
-    with conn_cinema.session as session_cinema:
-        result_cinema = session_cinema.execute('SELECT * FROM movie_schedule ORDER BY id;')
+        data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
         for row in result_cinema:
             id, movie_title_lama, genre_lama, director_lama, release_date_lama, start_time_lama, end_time_lama, theater_number_lama, ticket_price_lama = row
-            with st.expander(f'{movie_title_lama}'):
+            
+          with st.expander(f'{movie_title_lama}'):
                 with st.form(f'movie-data-{id}'):
                     movie_title_baru = st.text_input("Movie Title", movie_title_lama)
                     genre_baru = st.text_input("Genre", genre_lama)
